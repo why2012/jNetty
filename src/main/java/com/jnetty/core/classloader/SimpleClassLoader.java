a2bcaacd883343c628c45b634b3bfeaac02e3e85
why <529570509@qq.com>
2016-01-15 02:04:54 +0800

    classloader

---
+++ src/main/java/com/jnetty/core/classloader/SimpleClassLoader.java
@@ -0,0 +1,11 @@
+package com.jnetty.core.classloader;
+
+/**
+ * 
+ * @author wanghaiyang
+ *
+ * 加载servlet和lib, 每个Service一个ClassLoader, 对应Config中的servletClassLoader
+ */
+public class SimpleClassLoader {
+
+}
---
+++ src/main/java/com/jnetty/util/file/AppDetector.java
@@ -0,0 +1,12 @@
+package com.jnetty.util.file;
+
+/**
+ * 
+ * @author wanghaiyang
+ * 
+ *  根路径为JNettyBase
+ *	探测Webapp，解压缩WAR文件（JARFile），初始化ClassLoader，配置ServiceConfig
+ */
+public class AppDetector {
+
+}
