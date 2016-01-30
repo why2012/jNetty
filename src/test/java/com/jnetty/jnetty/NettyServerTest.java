62953d3ea85616d5211676792840560f876eb73f
why <529570509@qq.com>
2016-01-30 10:25:09 +0800

    test pid

--- src/test/java/com/jnetty/jnetty/NettyServerTest.java
+++ src/test/java/com/jnetty/jnetty/NettyServerTest.java
@@ -5,12 +5,15 @@ import com.jnetty.core.Config;
 import com.jnetty.core.Config.ServiceConfig;
 import com.jnetty.util.log.JNettyLogger;
 
+import java.io.*;
+import java.lang.management.ManagementFactory;
+
 public class NettyServerTest {
-	
+
 	public static void main(String[] args) throws Exception {
 		new NettyServerTest().testNettyServer();
 	}
-	
+
 	public void testNettyServer() throws Exception {
 		JNettyLogger.debug = false;
 		JNettyLogger.printStackTrace = true;
@@ -23,12 +26,37 @@ public class NettyServerTest {
 		sconfig.WebAppName = "transaction";
 		sconfig.staticResourceLoc = "/resources/";
 		config.serviceConfig.add(sconfig);
-		
+
 		Bootstrap bootstrap = new Bootstrap();
 		bootstrap.setConfig(config);
 		bootstrap.initialize();
 		System.out.println(sconfig);
 
+		//kill -TERM `cat sconfig.JNettyBase + "/" + sconfig.WebAppName + "/" + sconfig.WebAppName + ".pid"`
+		savePidFile(sconfig.JNettyBase + "/" + sconfig.WebAppName + "/" + sconfig.WebAppName + ".pid");
+
+		addShutdownHandler();
+
 		bootstrap.start();
+	}
+
+	private void savePidFile(String path) throws IOException {
+		File pidFile = new File(path);
+		if (!pidFile.exists()) {
+			pidFile.createNewFile();
+		}
+		PrintWriter writer = new PrintWriter(new FileWriter(pidFile));
+		String processName = ManagementFactory.getRuntimeMXBean().getName();
+		String pid = processName.split("@")[0];
+		writer.print(pid);
+		writer.close();
+	}
+
+	private void addShutdownHandler() {
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			public void run() {
+				JNettyLogger.log("Shutdown.");
+			}
+		});
 	}
 }
